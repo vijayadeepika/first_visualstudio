ecc2b8ad54cc2a844166e2e9696e3901f4da9eef
VIJAYA DEEPIKA <VIJAYA DEEPIKA@LAPTOP-V1NJNRBF>
2020-01-26 21:05:28 +0530

    Initial Commit

---
+++ TimeSheetPractice/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Owin;
+using Microsoft.Owin;
+using System;
+
+[assembly: OwinStartupAttribute(typeof(TimeSheetPractice.Startup))]
+namespace TimeSheetPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+            //app.MapSignalR();
+
+        }
+
+      
+    }
+}
